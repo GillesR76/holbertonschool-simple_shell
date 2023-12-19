b22ed1f123986db9ec2e949c42643be43dbcc16b
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-19 08:04:01 -0800

    uploading new versions of .c and .h files for the simple shell

--- shell.c
+++ shell.c
@@ -18,12 +18,8 @@ int main(void)
 	size_t commandSize = 0;
 	ssize_t bytesRead;
 
-	print_debug("[Info] -> Starting program");
-
 	while (1)
 	{
-		print_debug("[Info] -> Waiting for user input");
-
 		if (isatty(STDIN_FILENO) == 1)
 			printf("$ ");
 		else if (isatty(STDIN_FILENO) == -1)
@@ -38,20 +34,17 @@ int main(void)
 		{
 			if (feof(stdin))
 			{
-				print_debug("[Info] -> Exiting program");
+				printf("\n");
 				free(command);
 				exit(EXIT_SUCCESS);
 			}
-			print_debug("[Error] -> main() -> byteRead invalid");
 			free(command);
 			perror("Error");
 			exit(EXIT_FAILURE);
 		}
-		print_debug("[Success] -> byteRead valid");
 		command[bytesRead - 1] = '\0';
 		exec(&command);
 	}
-	print_debug("[Info] Stopping loop");
 	free(command);
 	return (0);
 }
