a5572d23f16339f4756d3e398bbc093665683c78
Gilles Richard <gilles-richard76@hotmail.com>
2023-12-20 13:37:25 -0800

    updating the shell function that calls the new created functions

--- shell.c
+++ shell.c
@@ -1,50 +1,26 @@
 #define _GNU_SOURCE
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 /**
- * main - super simple shell function
+ * main - simple shell function
  *
- * Return: void
+ * Return: 0
  */
 
 int main(void)
 {
 	char *command = NULL;
-	size_t commandSize = 0;
-	ssize_t bytesRead;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO) == 1)
+		if (isatty(STDIN_FILENO))
+		{
 			printf("$ ");
-		else if (isatty(STDIN_FILENO) == -1)
-		{
-			perror("isatty");
-			free(command);
-			exit(EXIT_FAILURE);
 		}
-
-		bytesRead = getline(&command, &commandSize, stdin);
-		if (bytesRead == -1)
-		{
-			if (feof(stdin))
-			{
-				printf("\n");
-				free(command);
-				exit(EXIT_SUCCESS);
-			}
-			free(command);
-			perror("Error");
-			exit(EXIT_FAILURE);
-		}
-		command[bytesRead - 1] = '\0';
-		exec(&command);
-	}
+	fflush(stdout);
+	command = readCommand(command);
+	exec(command);
 	free(command);
+	}
 	return (0);
 }
